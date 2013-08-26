7d50c6c8b4c6bc40f04c27e79d57df8a240aca17
Matijs van Zuijlen <matijs@matijs.net>
2013-08-26 19:53:52 +0200

    Implement three more methods on Cairo::Context

--- lib/gir_ffi-cairo/context.rb
+++ lib/gir_ffi-cairo/context.rb
@@ -7,14 +7,32 @@ module Cairo
       self.wrap ptr
     end
 
+    def arc xc, yc, radius, angle1, angle2
+      Lib.cairo_arc self, xc, yc, radius, angle1, angle2
+    end
+
+    def fill
+      Lib.cairo_fill self
+    end
+
     def get_target
       ptr = Lib.cairo_get_target self
       Surface.wrap ptr
+    end
+
+    def set_source_rgba red, green, blue, alpha
+      Lib.cairo_set_source_rgba self, red, green, blue, alpha
     end
   end
 
   module Lib
     attach_function :cairo_create, [:pointer], :pointer
     attach_function :cairo_get_target, [:pointer], :pointer
+
+    attach_function :cairo_arc,
+      [:pointer, :double, :double, :double, :double, :double], :void
+    attach_function :cairo_fill, [:pointer], :void
+    attach_function :cairo_set_source_rgba,
+      [:pointer, :double, :double, :double, :double], :void
   end
 end
--- test/integration/context_test.rb
+++ test/integration/context_test.rb
@@ -5,5 +5,15 @@ describe Cairo::Context do
     dst = Cairo::ImageSurface.create(:argb32, 400, 300)
     ctx = Cairo::Context.create(dst)
     ctx.get_target.to_ptr.must_equal dst.to_ptr
+    pass
+  end
+
+  it "can create and fill a path" do
+    dst = Cairo::ImageSurface.create(:argb32, 400, 300)
+    ctx = Cairo::Context.create(dst)
+    ctx.arc(300, 200, 100, 0, 2 * Math::PI)
+    ctx.set_source_rgba(1, 1, 0, 0.5)
+    ctx.fill
+    pass
   end
 end
