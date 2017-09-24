5dcc33304da2d47c74d8944657b56925b9cf14d2
Matijs van Zuijlen <matijs@matijs.net>
2017-09-24 17:32:51 +0200

    Autocorrect Style/StringLiterals

--- test/gir_ffi-cairo/pdf_surface_test.rb
+++ test/gir_ffi-cairo/pdf_surface_test.rb
@@ -1,20 +1,20 @@
 require File.expand_path('../test_helper.rb', File.dirname(__FILE__))
 
 describe Cairo::PDFSurface do
-  it "is a subclass of Cairo::Surface" do
+  it 'is a subclass of Cairo::Surface' do
     Cairo::PDFSurface.superclass.must_equal Cairo::Surface
   end
 
-  describe ".create" do
+  describe '.create' do
     before { @path = 'test.pdf' }
     after  { File.delete(@path) if File.exist? @path }
 
-    it "creates a new Cairo::PDFSurface" do
+    it 'creates a new Cairo::PDFSurface' do
       obj = Cairo::PDFSurface.create(@path, 300, 200)
       obj.must_be_instance_of Cairo::PDFSurface
     end
 
-    it "creates a new Cairo::PDFSurface with block" do
+    it 'creates a new Cairo::PDFSurface with block' do
       called = nil
       Cairo::PDFSurface.create(@path, 300, 200) {|surface|
         surface.must_be_instance_of Cairo::PDFSurface
--- test/integration/context_test.rb
+++ test/integration/context_test.rb
@@ -1,14 +1,14 @@
 require File.expand_path('../test_helper.rb', File.dirname(__FILE__))
 
 describe Cairo::Context do
-  it "can retrieve the surface it was created for" do
+  it 'can retrieve the surface it was created for' do
     dst = Cairo::ImageSurface.create(:argb32, 400, 300)
     ctx = Cairo::Context.create(dst)
     ctx.get_target.to_ptr.must_equal dst.to_ptr
     pass
   end
 
-  it "can create and fill a path" do
+  it 'can create and fill a path' do
     dst = Cairo::ImageSurface.create(:argb32, 400, 300)
     ctx = Cairo::Context.create(dst)
     ctx.arc(300, 200, 100, 0, 2 * Math::PI)
@@ -21,7 +21,7 @@ describe Cairo::Context do
     before { @path = 'integration-test.pdf' }
     after  { File.delete(@path) if File.exist? @path }
 
-    it "can create pdf" do
+    it 'can create pdf' do
       Cairo::PDFSurface.create(@path, 400, 300) {|surface|
         ctx = Cairo::Context.create(surface)
         ctx.arc(300, 200, 100, 0, 2 * Math::PI)
